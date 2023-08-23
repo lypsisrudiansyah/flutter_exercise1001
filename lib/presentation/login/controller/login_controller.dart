4f1364022da3a11a96f7ec4e22636bcf9a525878
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-08-23 22:09:24 +0700

    🔧 fix(main.dart): change home widget from Scaffold to LoginView 🔧 fix(login_controller.dart): add email and password variables 🔧 fix(login_controller.dart): initialize instance variable in initState 🔧 fix(login_controller.dart): remove dispose method 🔧 fix(login_view.dart): add TextFormField for email and password 🔧 fix(login_view.dart): add ElevatedButton for login functionality

--- lib/presentation/login/controller/login_controller.dart
+++ lib/presentation/login/controller/login_controller.dart
@@ -1,23 +1,26 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_basic_widget_exercise/core.dart';
 import '../view/login_view.dart';
 
 class LoginController extends State<LoginView> {
-    static late LoginController instance;
-    late LoginView view;
+  static late LoginController instance;
+  late LoginView view;
+  String email = "";
+  String password = "";
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
-    }
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
 
-    @override
-    void dispose() => super.dispose();
+  @override
+  void dispose() => super.dispose();
 
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
-}
-        
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+
+  Future doLogin() {
     
+  }
+}
--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,31 +1,85 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_basic_widget_exercise/core.dart';
 import '../controller/login_controller.dart';
 
 class LoginView extends StatefulWidget {
-    const LoginView({Key? key}) : super(key: key);
+  const LoginView({Key? key}) : super(key: key);
 
-    Widget build(context, LoginController controller) {
+  Widget build(context, LoginController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text("Login"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              TextFormField(
+                initialValue: 'admin@gmail.com',
+                maxLength: 20,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.blueGrey,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.email,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  helperText: 'Enter your email address',
+                ),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                initialValue: '123456',
+                maxLength: 20,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Colors.blueGrey,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.password,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  helperText: 'Enter your password',
+                ),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.login),
+                label: const Text("Login"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 162, 172, 176),
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50.0,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
-    }
+  }
 
-    @override
-    State<LoginView> createState() => LoginController();
+  @override
+  State<LoginView> createState() => LoginController();
 }
-    
